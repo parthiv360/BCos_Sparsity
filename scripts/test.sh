ea5f82be147c19cb3ef6d58afd82e3b18aedfe4b
Parthiv Sarkar <kakarot@Parthivs-MacBook-Pro-3.local>
2026-07-08 18:47:24 +0200

    Script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 SCRIPT_NAME="setup_model.py"
-CONDA_ENV_NAME="agentic-eval"
+CONDA_ENV_NAME="base"  # Use the base environment
 
 PROJECT_DIR="/home/pasa00007/Hiwi/BCos_Sparsity/"
 
 # Initialize Conda
 source "/scratch/compuling/pasa00007/miniconda3/etc/profile.d/conda.sh"
 
-# Activate the Conda environment
+# Activate the Conda base environment
 conda activate "$CONDA_ENV_NAME"
 
+# Navigate to the project directory
 cd "$PROJECT_DIR" || { echo "Failed to change directory to $PROJECT_DIR"; exit 1; }
 
 echo "=========================================="
