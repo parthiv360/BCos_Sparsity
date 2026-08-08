061805512a6a538e270bf7321d4e6daf09971db1
Parthiv Sarkar <kakarot@Parthivs-MacBook-Pro-3.local>
2026-08-08 22:59:54 +0200

    Fixing eval_strategy

--- bcos_lm/train_model.py
+++ bcos_lm/train_model.py
@@ -232,7 +232,7 @@ def main():
         per_device_train_batch_size=args.batch_size,
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         per_device_eval_batch_size=args.batch_size,
-        evaluation_strategy="steps",
+        eval_strategy="steps",
         save_strategy="steps",
         save_steps=args.save_steps,
         save_total_limit=1,
