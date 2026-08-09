b78e57cb4b43a6336854781984afb26d1d8c0559
Parthiv Sarkar <kakarot@Parthivs-MacBook-Pro-3.local>
2026-08-09 16:51:00 +0200

    Training for 5 epochs

--- scripts/train_bcos_gpt2.sh
+++ scripts/train_bcos_gpt2.sh
@@ -24,7 +24,7 @@ echo "=========================================="
     --gradient_accumulation_steps=1 \
     --max_seq_length=512 \
     --learning_rate=5e-04 \
-    --num_train_epochs=1 \
+    --num_train_epochs=5 \
     --warmup_steps_or_ratio=0.01 \
     --num_train_examples=500000 \
     --num_eval_examples=10000 \
