9dc81fa844e628fa83ea2bc91c38c33d51974eed
Parthiv Sarkar <kakarot@Parthivs-MacBook-Pro-3.local>
2026-08-13 23:16:00 +0200

    Vanilla GPT-2 training

--- scripts/train_bcos_gpt2.sh
+++ scripts/train_bcos_gpt2.sh
@@ -19,7 +19,7 @@ echo "=========================================="
 "$CONDA_PYTHON" -m "$MODULE_NAME" \
     --model_name_or_path gpt2 \
     --dataset_name="webtext" \
-    --output_dir "bcos_gpt2_epoch" \
+    --output_dir "vanilla_gpt2_epoch" \
     --batch_size=16 \
     --gradient_accumulation_steps=1 \
     --max_seq_length=512 \
@@ -30,7 +30,7 @@ echo "=========================================="
     --num_eval_examples=10000 \
     --seed=42 \
     --b 1.1 \
-    --bcos
+    
 
 echo "=========================================="
 echo "Training Completed"
