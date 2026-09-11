3b55f80de718984b23034f7fed0abb6c32367c58
Parthiv Sarkar <kakarot@Parthivs-MacBook-Pro-3.local>
2026-09-11 19:23:05 +0530

    Attribution Patching

--- utils.py
+++ utils.py
@@ -14,3 +14,16 @@ def get_logit_diff(model, tokenizer, prompt, correct, incorrect,device):
             logits = outputs.logits[:, -1, :]
         
         return logits[0, correct_token[0, 0]].item() - logits[0, incorrect_token[0, 0]].item()
+
+def get_logit_diff_with_grad(logits, tokenizer, correct, incorrect, device):
+    """
+    Computes the logit difference between the correct and incorrect tokens for a given prompt.
+    This function is used for attribution patching.
+    """
+
+    correct_token = tokenizer(" " + correct, return_tensors='pt').input_ids.to(device)
+    incorrect_token = tokenizer(" " + incorrect, return_tensors='pt').input_ids.to(device)
+
+    last_token_logits = logits[0, -1, :]
+
+    return last_token_logits[correct_token[0, 0]] - last_token_logits[incorrect_token[0, 0]]
