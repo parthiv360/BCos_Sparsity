51ce21279ee47d555485c1cf0ddde4b58b7a97f9
Parthiv Sarkar <kakarot@asapool2300.vpn.uni-saarland.de>
2026-09-15 19:50:28 +0530

    Gen Result

--- scripts/path_patching.sh
+++ scripts/path_patching.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 SCRIPT_NAME="path_patching.py"
 CONDA_ENV_NAME="base"  # Use the base environment
@@ -17,7 +18,19 @@ echo "Conda Environment: $CONDA_ENV_NAME"
 echo "=========================================="
 
 "$CONDA_PYTHON" -m "$MODULE_NAME" \
-    --checkpoint "vanilla_gpt2_epoch/checkpoint-156250" \
+    --checkpoint "vanilla_gpt2_epoch/checkpoint-156250" 
+
+"$CONDA_PYTHON" -m "$MODULE_NAME" \
+    --checkpoint "bcos_gpt2_epoch/checkpoint-156250" 
+
+"$CONDA_PYTHON" -m "$MODULE_NAME" \
+    --checkpoint "bcos_gpt2/b_1.25/checkpoint-156250" 
+
+"$CONDA_PYTHON" -m "$MODULE_NAME" \
+    --checkpoint "bcos_gpt2/b_1.5/checkpoint-156250" 
+
+"$CONDA_PYTHON" -m "$MODULE_NAME" \
+    --checkpoint "bcos_gpt2/b_2.0/checkpoint-156250" " 
 
 echo "=========================================="
 echo "Path Patching Completed"
